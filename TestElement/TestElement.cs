ff0bc7bb478010bdefbe4ac9f79e26aa3961f27d
qwerty34-qw <wh861440@gmail.com>
2021-02-06 23:45:14 +0200

    Add project files.

---
+++ TestElement/TestElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestElement
+{
+    public class TestElement
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+        public string CorectAnswer { get; set; }
+    }
+}
